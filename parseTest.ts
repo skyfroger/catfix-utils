30e9c71c24ad77a46808b5f8bd6994f442146f36
skyfroger <skyfroger@gmail.com>
2024-02-14 22:43:58 +0300

    dev: тесты для Параллельности

--- parseTest.ts
+++ parseTest.ts
@@ -7,10 +7,12 @@ import {
     clonesUsed,
     customBlockWithParams,
     longScriptNoComments,
+    onLoudnessOnBackChange,
+    onMessage,
 } from "./mock_projects/index.ts";
 
-const p = longScriptNoComments as unknown;
+const p = onLoudnessOnBackChange as unknown;
 const parsedP = parseProject(p as ScratchProject);
 
-//console.log(grader(parsedP));
-console.log(scanForWarnings(parsedP, p as ScratchProject));
+console.log(grader(parsedP));
+//console.log(scanForWarnings(parsedP, p as ScratchProject));
