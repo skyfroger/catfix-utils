0ca7c31231ad51427ee168fb8157708567c98a59
skyfroger <skyfroger@gmail.com>
2024-06-24 20:50:24 +0300

    feat: количество валидных скриптов

--- src/graders/utils.test.ts
+++ src/graders/utils.test.ts
@@ -1,31 +1,43 @@
 import { ScratchProject } from "../scratch";
-import { aliveBlocks } from "../../mock_projects/aliveBlocks";
+import { aliveBlocks, multipleScripts } from "../../mock_projects/aliveBlocks";
 
-import { isBlockAlive } from "./utils";
+import { isBlockAlive, validScriptsCount } from "./utils";
 
-describe("Тестирование функции isBlockAlive - блок в живом скрипте", () => {
-  const p = aliveBlocks as unknown;
-  const jsonProject = p as ScratchProject;
-  const target = jsonProject.targets[1];
+const p = aliveBlocks as unknown;
+const jsonProject = p as ScratchProject;
+const target = jsonProject.targets[1];
 
-  test("Скрипт существует, начинаем с шапки", () => {
-    expect(isBlockAlive(target, "1?9fU3?-B[s}O%,:3ZEN")).toBe(true);
-  });
+const j = multipleScripts as unknown;
+const jsonProjectMultiple = j as ScratchProject;
 
-  test("Скрипт существует, начинаем с вложенного блока", () => {
-    expect(isBlockAlive(target, "r+s1^O$y-6%p-$aK?_W1")).toBe(true);
-  });
+describe("Функция isBlockAlive - блок в живом скрипте", () => {
+    test("Скрипт существует, начинаем с шапки", () => {
+        expect(isBlockAlive(target, "1?9fU3?-B[s}O%,:3ZEN")).toBe(true);
+    });
 
-  test("Скрипт не существует, одинокая шапка", () => {
-    expect(isBlockAlive(target, ";C:ayf%[gA_-qAcW_cv/")).toBe(false);
-  });
+    test("Скрипт существует, начинаем с вложенного блока", () => {
+        expect(isBlockAlive(target, "r+s1^O$y-6%p-$aK?_W1")).toBe(true);
+    });
 
-  test("Скрипт не существует, одиночный блок", () => {
-    expect(isBlockAlive(target, "Int#0SK-``}TuR9Zj1kY")).toBe(false);
-  });
+    test("Скрипт не существует, одинокая шапка", () => {
+        expect(isBlockAlive(target, ";C:ayf%[gA_-qAcW_cv/")).toBe(false);
+    });
 
-  test("Скрипт не существует, набор блоков", () => {
-    expect(isBlockAlive(target, "X`eahZ#}k)Dk9WG!6*vO")).toBe(false);
-  });
+    test("Скрипт не существует, одиночный блок", () => {
+        expect(isBlockAlive(target, "Int#0SK-``}TuR9Zj1kY")).toBe(false);
+    });
 
+    test("Скрипт не существует, набор блоков", () => {
+        expect(isBlockAlive(target, "X`eahZ#}k)Dk9WG!6*vO")).toBe(false);
+    });
+});
+
+describe("Функция validScriptsCount - количество скриптов в проекте", () => {
+    test("В проекте есть 1 валидный скрипт", () => {
+        expect(validScriptsCount(jsonProject)).toBe(1);
+    });
+
+    test("В проекте есть 2 валидных скрипта", () => {
+        expect(validScriptsCount(jsonProjectMultiple)).toBe(2);
+    });
 });
--- src/graders/utils.ts
+++ src/graders/utils.ts
@@ -1,7 +1,6 @@
-import { Target } from "../scratch";
+import { Target, ScratchProject } from "../scratch";
 
 import { HAT_BLOCKS } from "../parser";
-
 
 // заголовочные блоки
 // opcode procedures_definition - блок определения функции
@@ -11,38 +10,58 @@ const HATS = [...HAT_BLOCKS, "procedures_definition"];
  * Определяем, находится ли блок в "живом" скрипте
  * @param sp спрайт или сцена
  * @param blockId строка с идентификатором блока
+ * @returns блок в живом скрипте
  */
-export function isBlockAlive(sp: Target, targetBlockId: string):boolean{
-    const blocks = sp.blocks; // объект с блоками
-    const block = blocks[targetBlockId];
+export function isBlockAlive(sp: Target, targetBlockId: string): boolean {
+  const blocks = sp.blocks; // объект с блоками
+  const block = blocks[targetBlockId];
 
-    // if (block.shadow) {
-    //     return true;
-    // }
+  // if (block.shadow) {
+  //     return true;
+  // }
 
-    // блок-шапка без наследников - мёртвый код
-    if (HATS.includes(block.opcode) && block.next === null) {
-        return false
+  // блок-шапка без наследников - мёртвый код
+  if (HATS.includes(block.opcode) && block.next === null) {
+    return false;
+  }
+
+  // у блока нет родителей и наследников
+  if (block.next === null && block.parent === null) {
+    return false;
+  }
+
+  // цепочка блоков не приводит к блоку шапке
+  let blockId: string | undefined = targetBlockId;
+  let lastBlockId: string | undefined = targetBlockId;
+  let opCode = "";
+  do {
+    opCode = blocks[blockId ?? ""]?.opcode;
+    lastBlockId = blockId;
+    blockId = blocks[blockId ?? ""]?.parent;
+  } while (blockId);
+
+  if (!HATS.includes(opCode)) {
+    return false;
+  }
+
+  return true;
+}
+
+/**
+ * Возвращает количество валидных скриптов в проекте
+ * @param jsonProject исходный проект
+ * @returns число скриптов
+ */
+export function validScriptsCount(jsonProject: ScratchProject): number {
+  let count = 0;
+  jsonProject.targets.forEach((target) => {
+    const blocks = target.blocks;
+    for (const key in blocks) {
+      if (HATS.includes(blocks[key].opcode) && isBlockAlive(target, key)) {
+        count++;
+      }
     }
+  });
 
-    // у блока нет родителей и наследников
-    if (block.next === null && block.parent === null) {
-        return false;
-    }
-
-    // цепочка блоков не приводит к блоку шапке
-    let blockId: string | undefined = targetBlockId;
-    let lastBlockId: string | undefined = targetBlockId;
-    let opCode = "";
-    do {
-        opCode = blocks[blockId ?? ""]?.opcode;
-        lastBlockId = blockId;
-        blockId = blocks[blockId ?? ""]?.parent;
-    } while (blockId);
-
-    if (!HATS.includes(opCode)) {
-        return false
-    }
-
-    return true;
+  return count;
 }
